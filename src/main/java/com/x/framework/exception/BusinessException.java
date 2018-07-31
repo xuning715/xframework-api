5dc9a5650d734de825451ec598e5276771a34f59
xuning715@163.com <xuning715@163.com>
2018-07-31 11:43:59 +0800

    x

--- src/main/java/com/x/framework/exception/BusinessException.java
+++ src/main/java/com/x/framework/exception/BusinessException.java
@@ -1,14 +1,11 @@
 package com.x.framework.exception;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BusinessException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-//	private static Logger logger = LoggerFactory.getLogger(BusinessException.class);
-    private static final Logger logger = LogManager.getLogger(BusinessException.class);
+	private static Logger logger = LoggerFactory.getLogger(BusinessException.class);
 
     public BusinessException() {
         super();
